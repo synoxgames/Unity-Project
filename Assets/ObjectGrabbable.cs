a483fd9ee5db2fe82506e8663709889e566cf042
BrooklynT101 <BrooklynIGTaylor@gmail.com>
2025-06-29 14:52:23 +1200

    Can now pickup and carry stuff, perfect for phat drops of dank furniture from your royal family to sell

---
+++ Assets/ObjectGrabbable.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGrabbable : MonoBehaviour
+{
+    [SerializeField] public bool debug = false;
+    [SerializeField] public float lerpSpeed = 10f; // Speed at which the object moves to the grab point
+    private Rigidbody objectRigidbody;
+    private Transform objectGrabPointTransform;
+
+    private void Awake()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+        if (objectRigidbody == null)
+        {
+            Debug.LogError("ObjectGrabbable requires a Rigidbody component.");
+        }
+    }
+    public void Grab(Transform objectGrabPointTransform)
+    {
+        if (debug) Debug.Log("Object grabbed: " + gameObject.name);
+        // changes the point to move to
+        this.objectGrabPointTransform = objectGrabPointTransform;
+        // disables the gravity of the object
+        objectRigidbody.useGravity = false;
+        // freezes the rotation of the object
+        // objectRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    public void Drop()
+    {
+        if (debug) Debug.Log("Object dropped: " + gameObject.name);
+        // resets the point to move to
+        this.objectGrabPointTransform = null;
+        // enables the gravity of the object
+        objectRigidbody.useGravity = true;
+        // unfreezes the rotation of the object
+        // objectRigidbody.constraints = RigidbodyConstraints.None;
+    }
+
+    private void FixedUpdate()
+    {
+        if (objectGrabPointTransform != null)
+        {
+            // lerp to smoothly move the object to the grab point
+            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.fixedDeltaTime * lerpSpeed);
+            objectRigidbody.MovePosition(newPosition);
+        }
+    }
+}
--- Assets/PlayerPickUpDrop.cs
+++ Assets/PlayerPickUpDrop.cs
@@ -5,42 +5,37 @@ using UnityEngine;
 public class PlayerPickUpDrop : MonoBehaviour
 {
     [SerializeField] private Transform playerCameraTransform;
-    private bool isHoldingItem = false;
-    private GameObject heldItem;
+
+    [SerializeField] private Transform objectGrabPointTransform;
+    [SerializeField] public bool debug = false;
+    private ObjectGrabbable objectGrabbable;
+
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !isHoldingItem)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            TryPickUpItem();
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && isHoldingItem)
-        {
-            DropItem();
-        }
-    }
-
-    private void TryPickUpItem()
-    {
-        if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit hit, 2f))
-        {
-            if (hit.collider.CompareTag("Furniture"))
+            if (objectGrabbable == null)
             {
-                PickUpItem(hit.collider.gameObject);
+                // carrying fuck all, try n grab
+                float pickUpDistance = 3f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance))
+                {
+                    if (debug) Debug.Log("Raycast hit: " + raycastHit.transform.name);
+                    if (raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        if (debug) Debug.Log("Picked up object: " + raycastHit.transform.name);
+                        objectGrabbable.Grab(objectGrabPointTransform);
+                    }
+                }
+            }
+            else
+            {
+                // currently raxing shit, try n drop
+                if (debug) Debug.Log("Dropped object: " + objectGrabbable.gameObject.name);
+                objectGrabbable.Drop();
+                objectGrabbable = null;
             }
         }
-    }
-
-    private void PickUpItem(GameObject item)
-    {
-        isHoldingItem = true;
-        heldItem = item;
-        Debug.Log("Picked up: " + item.name);
-    }
-
-    private void DropItem()
-    {
-        isHoldingItem = false;
-        heldItem.SetActive(true);
-        heldItem = null;
     }
 }
